5fa1fab12027b38fb36214e43adc3e77e925b2f1
SardineFish <Sardine@live.cn>
2018-11-07 16:49:11 +0800

    Add project SpriteSheet

---
+++ SpriteSheet/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpriteSheet
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        bool dragHold = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonClose_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void root_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(e.LeftButton == MouseButtonState.Pressed && dragHold)
+                this.DragMove();
+        }
+
+        private void buttonMin_Click(object sender, RoutedEventArgs e) => WindowState = WindowState.Minimized;
+
+        private void root_MouseDown(object sender, MouseButtonEventArgs e) => dragHold = true;
+
+        private void root_MouseUp(object sender, MouseButtonEventArgs e) => dragHold = false;
+
+        private void editorWrapper_MouseDown(object sender, MouseButtonEventArgs e) => e.Handled = true;
+    }
+}
