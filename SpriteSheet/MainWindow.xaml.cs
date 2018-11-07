2e88dbd538f939416ff28515bbb7cf4a8b1a99f6
SardineFish <Sardine@live.cn>
2018-11-07 22:20:01 +0800

    Add sprite sheet preview

--- SpriteSheet/MainWindow.xaml.cs
+++ SpriteSheet/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using SardineFish.Windows.Controls;
 
 namespace SpriteSheet
 {
@@ -20,10 +21,83 @@ namespace SpriteSheet
     /// </summary>
     public partial class MainWindow : Window
     {
+        public SpriteSheetGenerator SpriteSheet;
         bool dragHold = false;
+        Vector RenderImageSize;
+        Vector PreviewImageSize;
+        int Columns = 0;
+        int Rows = 0;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        public void Update()
+        {
+            Columns = SpriteSheet.Columns;
+            if (switchMultiRow.SwitchStatus == SardineFish.Windows.Controls.SwitchStatus.Off)
+                Columns = SpriteSheet.Images.Count;
+            Rows = (int)Math.Ceiling((double)SpriteSheet.Images.Count / Columns);
+
+            RenderImageSize = new Vector(Columns * SpriteSheet.MaxWidth, Rows * SpriteSheet.MaxHeight);
+            if (switchSnap2Pow.SwitchStatus == SwitchStatus.On)
+            {
+                var size = (int)Math.Pow(2, Math.Ceiling(Math.Log(RenderImageSize.X, 2)));
+                RenderImageSize = new Vector(size, size);
+            }
+            PreviewImageSize = RenderImageSize;
+
+            images.Children.Clear();
+            double scale = 1;
+
+            if(switchMultiRow.SwitchStatus == SwitchStatus.On)
+            {
+                if (PreviewImageSize.X> workSpace.ActualWidth - 64)
+                {
+                    scale = (workSpace.ActualWidth - 64) / RenderImageSize.X;
+                    PreviewImageSize *= scale;
+                }
+            }
+            else
+            {
+                if (PreviewImageSize.Y > workSpace.ActualHeight - 128)
+                {
+                    scale = (workSpace.ActualHeight - 128) / RenderImageSize.Y;
+                    PreviewImageSize *= scale;
+                }
+            }
+            images.Width = PreviewImageSize.X;
+            images.Height = PreviewImageSize.Y;
+
+            var centerOffset = new Vector(workSpace.ActualWidth - PreviewImageSize.X, workSpace.ActualHeight - PreviewImageSize.Y);
+            centerOffset /= 2;
+            if (switchMultiRow.SwitchStatus == SwitchStatus.On && PreviewImageSize.Y > workSpace.ActualHeight)
+            {
+                centerOffset.Y += (PreviewImageSize.Y - workSpace.ActualHeight) / 2 + 32;
+            }
+            else if(switchMultiRow.SwitchStatus == SwitchStatus.Off && PreviewImageSize.X > workSpace.ActualWidth)
+            {
+                centerOffset.X += (PreviewImageSize.X - workSpace.ActualWidth) / 2 + 64;
+            }
+            imagesTransform.X = centerOffset.X;
+            imagesTransform.Y = centerOffset.Y;
+
+            foreach (var img in SpriteSheet.Images)
+            {
+                var grid = new Grid();
+                grid.Width = SpriteSheet.MaxWidth * scale;
+                grid.Height = SpriteSheet.MaxHeight * scale;
+                var image = new Image();
+                image.Source = SpriteSheetGenerator.ToWriteableBitmap(img, SpriteSheet.MaxWidth, SpriteSheet.MaxHeight);
+                image.SetValue(RenderOptions.BitmapScalingModeProperty, BitmapScalingMode.HighQuality);
+                image.SetValue(RenderOptions.EdgeModeProperty, EdgeMode.Unspecified);
+                image.SnapsToDevicePixels = true;
+                image.Width = SpriteSheet.MaxWidth * scale;
+                image.Height = SpriteSheet.MaxHeight * scale;
+                image.Stretch = Stretch.Fill;
+                grid.Children.Add(image);
+                images.Children.Add(grid);
+            }
         }
 
         private void buttonClose_Click(object sender, RoutedEventArgs e)
@@ -44,5 +118,41 @@ namespace SpriteSheet
         private void root_MouseUp(object sender, MouseButtonEventArgs e) => dragHold = false;
 
         private void editorWrapper_MouseDown(object sender, MouseButtonEventArgs e) => e.Handled = true;
+
+        private void editorWrapper_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+                e.Effects = DragDropEffects.Move;
+            else
+                e.Effects = DragDropEffects.None;
+        }
+
+        private void editorWrapper_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                var paths = (e.Data.GetData(DataFormats.FileDrop) as string[]);
+                SpriteSheet = new SpriteSheetGenerator(paths);
+                SpriteSheet.Load();
+                Update();
+                
+            }
+        }
+
+        private void switchMultiRow_SwitchChanged(object sender, EventArgs e)
+        {
+            if (switchMultiRow.SwitchStatus == SwitchStatus.Off)
+                switchSnap2Pow.SwitchStatus = SwitchStatus.Off;
+            Update();
+        }
+
+        private void switchReverse_SwitchChanged(object sender, EventArgs e) => Update();
+
+        private void switchSnap2Pow_SwitchChanged(object sender, EventArgs e)
+        {
+            if (switchSnap2Pow.SwitchStatus == SwitchStatus.On)
+                switchMultiRow.SwitchStatus = SwitchStatus.On;
+            Update();
+        }
     }
 }
---
+++ SpriteSheet/SpriteSheetGenerator.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkiaSharp;
+using System.IO;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+
+namespace SpriteSheet
+{
+    public class SpriteSheetGenerator
+    {
+        string[] Paths;
+        public List<SKBitmap> Images = new List<SKBitmap>();
+        public int MaxWidth => Images.Max(img => img.Width);
+        public int MaxHeight => Images.Max(img => img.Height);
+        public int Columns => (int)Math.Ceiling(Math.Sqrt((double)Images.Count * MaxHeight / MaxWidth));
+
+        public SpriteSheetGenerator(string[] paths)
+        {
+            Paths = paths.OrderBy(p => p).ToArray();
+        }
+
+        public void Load()
+        {
+            foreach (var path in Paths)
+            {
+                SKBitmap bitmap = null;
+
+                using (var fs = new FileStream(path, FileMode.Open))
+                {
+                    bitmap = SKBitmap.Decode(fs);
+                    fs.Close();
+                }
+                Images.Add(bitmap);
+            }
+        }
+
+        // Reference http://lostindetails.com/blog/post/SkiaSharp-with-Wpf
+        public static WriteableBitmap ToWriteableBitmap(SKBitmap img, int width, int height)
+        {
+            var bitmap = new WriteableBitmap(width, height, 96, 96, PixelFormats.Bgra32, BitmapPalettes.Halftone256Transparent);
+            bitmap.Lock();
+            using (var surface = SKSurface.Create(width:width,height:height,colorType:SKColorType.Bgra8888, alphaType:SKAlphaType.Premul,pixels:bitmap.BackBuffer,rowBytes: width * 4))
+            {
+                var canvas = surface.Canvas;
+                canvas.DrawBitmap(img, 0, 0);
+            }
+            bitmap.AddDirtyRect(new System.Windows.Int32Rect(0, 0, width, height));
+            bitmap.Unlock();
+            return bitmap;
+        }
+    }
+}
